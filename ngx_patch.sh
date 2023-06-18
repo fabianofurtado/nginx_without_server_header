a58e56f97cc204fe80f7d6a690221ebd5724913c
Fabiano Furtado <fabianofurtado@gmail.com>
2023-06-17 21:03:49 -0300

    nginx 1.24.0 without server header

--- ngx_patch.sh
+++ ngx_patch.sh
@@ -1,11 +1,6 @@
 #!/usr/bin/sh
 
 NGX_VERSION='nginx-1.24.0'
-
 wget https://nginx.org/download/$NGX_VERSION.tar.gz
 tar -xf $NGX_VERSION.tar.gz
-patch -d $NGX_VERSION/ -Np0 < ngx_http_core_module.c.patch
-patch -d $NGX_VERSION/ -Np0 < ngx_http_core_module.h.patch
-patch -d $NGX_VERSION/ -Np0 < ngx_http_header_filter_module.c.patch
-patch -d $NGX_VERSION/ -Np0 < ngx_http_special_response.c.patch
-patch -d $NGX_VERSION/ -Np0 < ngx_http_v2_filter_module.c.patch
+for PATCH_FILE in *.patch; { patch -d $NGX_VERSION/ -Np0 < $PATCH_FILE; }
